b9be646f1ab5392c4cea268a9130d344873281f5
Aztec0 <atupota17@gmail.com>
2022-11-13 01:19:43 +0200

    fix views and setting parameters

--- app/lib/controller.rb
+++ app/lib/controller.rb
@@ -3,33 +3,33 @@ module Controller
       Rack::Response.new do |response|
         case name
         when 'health'
-          response.set_cookie('health', req.cookies['health'].to_i + 1)
-          response.set_cookie('food', req.cookies['food'].to_i + 10)
-          response.set_cookie('energy', req.cookies['energy'].to_i - 5)
+          response.set_cookie('health', req.cookies['health'].to_i + rand(0..10))
+          response.set_cookie('food', req.cookies['food'].to_i + rand(0..10))
+          response.set_cookie('energy', req.cookies['energy'].to_i - rand(0..5))
         when 'food'
-          response.set_cookie('food', req.cookies['food'].to_i + 10)
-          response.set_cookie('energy', req.cookies['energy'].to_i + 3)
-          response.set_cookie('cleanness', req.cookies['cleanness'].to_i - 5)
+          response.set_cookie('food', req.cookies['food'].to_i + rand(0..10))
+          response.set_cookie('cleanness', req.cookies['cleanness'].to_i - rand(0..10))
+          response.set_cookie('energy', req.cookies['energy'].to_i - rand(0..5))
         when 'energy'
-          response.set_cookie('energy', req.cookies['energy'].to_i + 20)
-          response.set_cookie('happiness', req.cookies['hapinness'].to_i + 10)
-          response.set_cookie('health', req.cookies['health'].to_i - 5)
-          response.set_cookie('cleanness', req.cookies['cleanness'].to_i - 5)
-          response.set_cookie('food', req.cookies['food'].to_i - 5)
+          response.set_cookie('energy', req.cookies['energy'].to_i + rand(0..20))
+          response.set_cookie('happiness', req.cookies['happiness'].to_i - rand(0..10))
+          response.set_cookie('health', req.cookies['health'].to_i + rand(-5..5))
+          response.set_cookie('cleanness', req.cookies['cleanness'].to_i - rand(0..10))
+          response.set_cookie('food', req.cookies['food'].to_i - rand(0..10))
         when 'happiness'
-          response.set_cookie('happiness', req.cookies['hapinness'].to_i + 10)
-          response.set_cookie('cleanness', req.cookies['cleanness'].to_i - 5)
-  
+          response.set_cookie('happiness', req.cookies['happiness'].to_i + rand(0..10))
+          response.set_cookie('cleanness', req.cookies['cleanness'].to_i - rand(0..5))
+          response.set_cookie('energy', req.cookies['energy'].to_i - rand(0..5))
         when 'cleanness'
-          response.set_cookie('cleanness', req.cookies['cleanness'].to_i - 5)
-          response.set_cookie('energy', req.cookies['energy'].to_i - 5)
-          response.set_cookie('health', req.cookies['health'].to_i + 1)
+          response.set_cookie('cleanness', req.cookies['cleanness'].to_i + rand(0..10))
+          response.set_cookie('health', req.cookies['health'].to_i + rand(0..10))
+          response.set_cookie('energy', req.cookies['energy'].to_i - rand(0..5))
         when 'spotting'
-            response.set_cookie('cleanness', req.cookies['cleanness'].to_i - 5)
-            response.set_cookie('energy', req.cookies['energy'].to_i - 5)
-            response.set_cookie('health', req.cookies['health'].to_i + 1)  
-            response.set_cookie('food', req.cookies['food'].to_i - 5)
-            response.set_cookie('happiness', req.cookies['hapinness'].to_i + 10)
+            response.set_cookie('cleanness', req.cookies['cleanness'].to_i + rand(-10..10))
+            response.set_cookie('energy', req.cookies['energy'].to_i + rand(-10..10))
+            response.set_cookie('health', req.cookies['health'].to_i + rand(-10..10))  
+            response.set_cookie('food', req.cookies['food'].to_i + rand(-10..10))
+            response.set_cookie('happiness', req.cookies['happiness'].to_i + rand(-10..10))
         end
   
         response.redirect('/start')
